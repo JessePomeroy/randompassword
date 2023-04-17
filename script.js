61548c12e31e750e5702c061954e9739c47ea5f3
JessePomeroy <thinkingofview@gmail.com>
2023-04-17 17:26:43 -0500

    tweaks

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var characterLength = 8;
-var choice = [];
-var character = ["!", "@", "#", "$", "%", "^", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "~", "-", "_", "=", "?", "[", "]", "{", "}"]
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let characterLength = 8;
+let choice = [];
+let character = ["!", "@", "#", "$", "%", "^", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "~", "-", "_", "=", "?", "[", "]", "{", "}"]
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var confPrompts = usrPrompts();
-  var passwordText = document.querySelector("#password");
+  let confPrompts = usrPrompts();
+  let passwordText = document.querySelector("#password");
 
   if (confPrompts) {
     var password = generatePassword();
@@ -30,7 +30,7 @@ function generatePassword() {
 
   var password = "";
   for (var i = 0; i < characterLength; i++) {
-    var randomCharacter = Math.floor(Math.random() * choice.length);
+    var randomCharacter = Math.floor(Math.random() * choice.length); // found the random integer method on MDN at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     password = password + choice[randomCharacter];
   }
   return password;
